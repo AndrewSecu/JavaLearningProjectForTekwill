3b2aef56722142c712fa027f4ef727b2dba39b3c
Andrei Secu <andrei.s3cu@gmail.com>
2024-10-14 19:54:31 +0300

    Added the Variables and Comments task

---
+++ src/main/java/com/andreisecuqa/variableandcomments/VariablesAndComments.java
@@ -0,0 +1,33 @@
+package com.andreisecuqa.variableandcomments;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+
+        // Declare section
+
+        boolean exampleOfBooleanVariable = true;
+        char exampleOfCharVariable;
+        int exampleOfIntVariable;
+        byte exampleOfByteVariable;
+        short exampleOfShortVariable;
+        long exampleOfLongVariable;
+        float exampleOfFloatVariable;
+        double exampleOfDoubleVariable;
+
+        // Intialization section
+
+        exampleOfCharVariable = 'C';
+        exampleOfIntVariable = 5646456;
+        exampleOfByteVariable = 12;
+        exampleOfShortVariable = 32000;
+        exampleOfLongVariable = 564564562;
+        exampleOfFloatVariable = 64.5f;
+        exampleOfDoubleVariable = 545.4564855445;
+
+        System.out.println(exampleOfShortVariable);
+
+        // CTRL + ALT + L - pretty code
+    }
+
+}
